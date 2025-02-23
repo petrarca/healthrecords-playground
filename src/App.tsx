4133b235a367968812ac302f84c77b8be7831cc9
Wolfgang Miller <wolfgang.miller@cgm.com>
2025-02-23 13:39:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-200">
+      <span className="text-4xl">Hello, enjoy coding...</span>
+    </div>
+  )
+}
+
+export default App
