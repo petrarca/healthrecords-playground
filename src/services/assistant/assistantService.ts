74d2b4a375c7f24cb5a646e77b2233cc5228276e
Wolfgang Miller <wolfgang.miller@cgm.com>
2025-03-02 00:03:48 +0100

    Greetings change in Thea

--- src/services/assistant/assistantService.ts
+++ src/services/assistant/assistantService.ts
@@ -144,7 +144,7 @@ class AssistantService {
 
   // Get the welcome message
   private getWelcomeMessage(): string {
-    return "👋 Welcome to <b>Thea</b>, your AI-powered assistant! Type \"help\" to see examples of what I can do.<p>PS: A more powerful version will be available soon.";
+    return "👋 Welcome to <b>Thea</b>, your AI-powered assistant! Type \"help\" to see examples of what I can do.<p>PS: A more powerful version is just around the corner.";
   }
 
   // Get current state
